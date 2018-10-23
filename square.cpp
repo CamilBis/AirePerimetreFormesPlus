606fec43061d5b66248731d4b4fd746dadfae41f
Camil Bisson <camilbisson@hotmail.com>
2018-10-23 08:44:54 -0400

    improvement(PerimetreAire): Improve square class

--- square.cpp
+++ square.cpp
@@ -2,7 +2,7 @@
 
 Square::Square(Point* A, Point* B, Point* C, Point* D): Rectangle(A,B,C,D){
     if(_measures[0] != _measures[1]){
-        std::cout << "Ceci n'est pas un carre!" << std::endl;
+        std::runtime_error("Ceci n'est pas un carre!");
         return;
     }
 }
